787a8e90cd3288cf74fdbc0257d8211408493301
vedant12358 <vedant12358@gmail.com>
2026-03-23 14:31:56 +0530

    Initial commit - UC1

---
+++ BookMyStayApp/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,23 @@
+
+public class UseCase1HotelBookingApp {
+
+    /**
+     * Application entry point.
+     *
+     * This method is the first method executed
+     * when the program is launched by the JVM.
+     *
+     * @param args Command-line arguments
+     */
+    public static void main(String[] args) {
+
+        // Display welcome message
+        System.out.println("=======================================");
+        System.out.println("   Welcome to Book My Stay Application ");
+        System.out.println("   Hotel Booking System v1.0           ");
+        System.out.println("=======================================");
+
+        // Confirm successful startup
+        System.out.println("Application started successfully!");
+    }
+}
